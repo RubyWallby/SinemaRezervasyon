4b9a58be6a77c88b5f482ce1b4a1570d5aabeb71
McanSahinbas <72572226+RubyWallby@users.noreply.github.com>
2020-12-06 15:52:37 +0300

    Initial commit

---
+++ AnaMenü.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace SinemaRezervasyon
+{
+    partial class AnaMenü
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbAd = new System.Windows.Forms.TextBox();
+            this.tbSoyad = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(304, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kral Sinema";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // tbAd
+            // 
+            this.tbAd.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbAd.ForeColor = System.Drawing.Color.DarkGreen;
+            this.tbAd.Location = new System.Drawing.Point(377, 146);
+            this.tbAd.Name = "tbAd";
+            this.tbAd.Size = new System.Drawing.Size(174, 22);
+            this.tbAd.TabIndex = 2;
+            this.tbAd.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbSoyad
+            // 
+            this.tbSoyad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.tbSoyad.ForeColor = System.Drawing.Color.DarkGreen;
+            this.tbSoyad.Location = new System.Drawing.Point(375, 209);
+            this.tbSoyad.Name = "tbSoyad";
+            this.tbSoyad.Size = new System.Drawing.Size(174, 22);
+            this.tbSoyad.TabIndex = 5;
+            this.tbSoyad.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Jokerman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(311, 394);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 32);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Bilet Al";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Jokerman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button2.Location = new System.Drawing.Point(430, 394);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(76, 32);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "İptal";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(758, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label2.Location = new System.Drawing.Point(211, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 41);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Adınız:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Jokerman", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(164, 195);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 41);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Soyadınız:";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.OliveDrab;
+            this.button4.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button4.Location = new System.Drawing.Point(47, 259);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(161, 58);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Filmler";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.OliveDrab;
+            this.button5.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button5.Location = new System.Drawing.Point(296, 259);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(182, 58);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Yiyecekler";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.OliveDrab;
+            this.button6.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button6.Location = new System.Drawing.Point(560, 259);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(174, 58);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "İçecekler";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.OliveDrab;
+            this.button7.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button7.Location = new System.Drawing.Point(175, 330);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(161, 58);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Koltuklar";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.OliveDrab;
+            this.button8.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button8.Location = new System.Drawing.Point(471, 330);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(161, 58);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Ödeme";
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // AnaMenü
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbSoyad);
+            this.Controls.Add(this.tbAd);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AnaMenü";
+            this.Text = "AnaMenü";
+            this.Load += new System.EventHandler(this.AnaMenü_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbAd;
+        private System.Windows.Forms.TextBox tbSoyad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
